c09aa06c6a9f9cf9bbc932b0c63a8b67bb36e9d7
Julio Matsoui <julio.matsoui@eldorado.org.br>
2023-02-13 11:34:02 -0300

    Added unitTest get user by email

--- deamanagement/src/main/java/br/com/dea/management/user/controller/UserController.java
+++ deamanagement/src/main/java/br/com/dea/management/user/controller/UserController.java
@@ -1,14 +1,13 @@
 package br.com.dea.management.user.controller;
 
 import br.com.dea.management.student.domain.Student;
+import br.com.dea.management.student.dto.StudentDto;
 import br.com.dea.management.student.service.StudentService;
 import br.com.dea.management.user.domain.User;
 import br.com.dea.management.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +22,18 @@ public class UserController {
         return this.userService.findAllUsers();
     }
 
+    @GetMapping("/user/{email}")
+    public User getUsers(@PathVariable String email) {
 
+        log.info(String.format("Fetching student by id : Id : %s", email));
+
+        User user = this.userService.findUserByEmail(email);
+
+        log.info(String.format("Student loaded successfully : Student : %s", user));
+
+        return user;
+
+    }
 
 }
+
